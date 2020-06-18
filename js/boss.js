085f7c4c6a4896618b4a658fdef57513de9faacf
Parkhojung <34734102+Parkhojung@users.noreply.github.com>
2020-06-18 22:04:34 +0900

    first commit

---
+++ js/boss.js
@@ -0,0 +1,22 @@
+<script>
+    function menu_click_fun(x){
+        // bar event
+        x.classList.toggle("change");
+
+        
+        // sub menu event
+        var sub_menu = document.querySelector(".sub-menu");
+        if(sub_menu.style.visibility == 'visible'){
+//            alert("change none");
+            
+            sub_menu.style.opacity = 0;
+            sub_menu.style.visibility ='hidden';
+        }else{
+            
+//            alert("change block");
+            
+            sub_menu.style.opacity = 1;
+            sub_menu.style.visibility = 'visible';
+        }
+    }
+</script>
