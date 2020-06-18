ac53da7344d13becfa6ff1df06dac1508876a313
Parkhojung <34734102+Parkhojung@users.noreply.github.com>
2020-06-18 22:41:40 +0900

    only pc version support

--- js/boss.js
+++ js/boss.js
@@ -1,22 +1,20 @@
-<script>
-    function menu_click_fun(x){
-        // bar event
-        x.classList.toggle("change");
 
-        
-        // sub menu event
-        var sub_menu = document.querySelector(".sub-menu");
-        if(sub_menu.style.visibility == 'visible'){
+function menu_click_fun(x){
+    // bar event
+    x.classList.toggle("change");
+
+    
+    // sub menu event
+    var sub_menu = document.querySelector(".sub-menu");
+    if(sub_menu.style.visibility == 'visible'){
 //            alert("change none");
-            
-            sub_menu.style.opacity = 0;
-            sub_menu.style.visibility ='hidden';
-        }else{
-            
+        
+        sub_menu.style.opacity = 0;
+        sub_menu.style.visibility ='hidden';
+    }else{
+        
 //            alert("change block");
-            
-            sub_menu.style.opacity = 1;
-            sub_menu.style.visibility = 'visible';
-        }
+        sub_menu.style.opacity = 1 ;
+        sub_menu.style.visibility = 'visible';
     }
-</script>
+}
